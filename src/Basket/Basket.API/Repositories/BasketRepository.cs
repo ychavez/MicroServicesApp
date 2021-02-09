3f728f1f649ac41b3fcd7ac34e9e441e7c91be62
Yael Chavez <you400@gmail.com>
2021-02-08 22:55:06 -0600

    basket

---
+++ src/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,47 @@
+﻿using Basket.API.Data;
+using Basket.API.Data.Interfaces;
+using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IBasketContext context;
+
+        public BasketRepository(IBasketContext context)
+        {
+            this.context = context;
+        }
+        public async Task<bool> DeleteBasket(string userName)
+        {
+            return await context.Redis.KeyDeleteAsync(userName);
+        }
+
+        public async Task<BasketCart> GetBasket(string userName)
+        {
+            var basket = await context.Redis.StringGetAsync(userName);
+            if (basket.IsNullOrEmpty)
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<BasketCart>(basket);
+        }
+
+        public async Task<BasketCart> UpdateBasket(BasketCart basket)
+        {
+            var updated = await context.Redis.StringSetAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            if (!updated)
+            {
+                return null;
+            }
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
---
+++ src/Basket/Basket.API/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,17 @@
+﻿using Basket.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<BasketCart> GetBasket(string UserName);
+
+        Task<BasketCart> UpdateBasket(BasketCart basket);
+
+        Task<bool> DeleteBasket(string userName);
+    }
+}
